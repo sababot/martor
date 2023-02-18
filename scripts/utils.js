13e1622cfaff92510519042b0cd39a8d87b44dc2
sababot <aitor@garciahombrados.com>
2023-02-19 00:46:19 +0100

    front/back products

---
+++ scripts/utils.js
@@ -0,0 +1,17 @@
+function hover_must_fall_shirt_1(element) {
+	element.setAttribute('src', 'images/collections/fall-to-rise/fall-to-rise-shirt-1-back.webp');
+}
+
+function unhover_must_fall_shirt_1(element) {
+	element.setAttribute('src', 'images/collections/fall-to-rise/fall-to-rise-shirt-1-front.webp');
+}
+
+//
+
+function hover_must_fall_shirt_2(element) {
+	element.setAttribute('src', 'images/collections/fall-to-rise/fall-to-rise-shirt-2-back.webp');
+}
+
+function unhover_must_fall_shirt_2(element) {
+	element.setAttribute('src', 'images/collections/fall-to-rise/fall-to-rise-shirt-2-front.webp');
+}
