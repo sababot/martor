832dbb57829c1bc3f180cf05999126bb6a7930a9
sababot <aitor@garciahombrados.com>
2023-02-19 21:26:52 +0100

    fall to rise beanie & shop improvements

--- scripts/utils.js
+++ scripts/utils.js
@@ -15,3 +15,23 @@ function hover_must_fall_shirt_2(element) {
 function unhover_must_fall_shirt_2(element) {
 	element.setAttribute('src', 'images/collections/fall-to-rise/fall-to-rise-shirt-2-front.webp');
 }
+
+//
+
+function hover_must_fall_hat_1(element) {
+	element.setAttribute('src', 'images/collections/fall-to-rise/fall-to-rise-hat-1.webp');
+}
+
+function unhover_must_fall_hat_1(element) {
+	element.setAttribute('src', 'images/collections/fall-to-rise/fall-to-rise-hat-1.webp');
+}
+
+//
+
+function hover_must_fall_hat_2(element) {
+	element.setAttribute('src', 'images/collections/fall-to-rise/fall-to-rise-hat-2.webp');
+}
+
+function unhover_must_fall_hat_2(element) {
+	element.setAttribute('src', 'images/collections/fall-to-rise/fall-to-rise-hat-2.webp');
+}
