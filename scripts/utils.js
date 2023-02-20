c288b530b8253e0bd17267b53be0a0b3dcf347cf
sababot <aitor@garciahombrados.com>
2023-02-20 12:22:59 +0100

    ticker & footer

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,3 +1,21 @@
+function header_ticker_1(){
+	document.getElementById('ticker').innerHTML = 'world wide shipping';
+	document.getElementById('ticker').style.marginLeft = '0vw';
+
+	setTimeout(header_ticker_2, 5000);
+}
+
+function header_ticker_2(){
+	document.getElementById('ticker').innerHTML = 'fall to rise out now';
+	document.getElementById('ticker').style.marginLeft = '89vw';
+
+	setTimeout(header_ticker_1, 5000);
+}
+
+header_ticker_1();
+
+//
+
 function hover_must_fall_shirt_1(element) {
 	element.setAttribute('src', 'images/collections/fall-to-rise/fall-to-rise-shirt-1-back.webp');
 }
