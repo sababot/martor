5e48df7efed96f1f603ada17401367f09e7278ea
sababot <aitor@garciahombrados.com>
2023-03-03 23:43:17 +0100

    hoodie 1

---
+++ scripts/products-shop.js
@@ -0,0 +1,51 @@
+//
+
+function hover_must_fall_shirt_1(element) {
+	element.setAttribute('src', '../images/collections/fall-to-rise/palace-mockups/fall-to-rise-shirt-1-back.webp');
+	element.setAttribute('id', 'card');
+}
+
+function unhover_must_fall_shirt_1(element) {
+	element.setAttribute('src', '../images/collections/fall-to-rise/palace-mockups/fall-to-rise-shirt-1-front.webp');
+}
+
+//
+
+function hover_must_fall_shirt_2(element) {
+	element.setAttribute('src', '../images/collections/fall-to-rise/palace-mockups/fall-to-rise-shirt-2-back.webp');
+}
+
+function unhover_must_fall_shirt_2(element) {
+	element.setAttribute('src', '../images/collections/fall-to-rise/palace-mockups/fall-to-rise-shirt-2-front.webp');
+}
+
+//
+
+function hover_must_fall_hat_1(element) {
+	element.setAttribute('src', '../images/collections/fall-to-rise/palace-mockups/fall-to-rise-hat-1.webp');
+}
+
+function unhover_must_fall_hat_1(element) {
+	element.setAttribute('src', '../images/collections/fall-to-rise/palace-mockups/fall-to-rise-hat-1.webp');
+}
+
+//
+
+function hover_must_fall_hat_2(element) {
+	element.setAttribute('src', '../images/collections/fall-to-rise/palace-mockups/fall-to-rise-hat-2.webp');
+}
+
+function unhover_must_fall_hat_2(element) {
+	element.setAttribute('src', '../images/collections/fall-to-rise/palace-mockups/fall-to-rise-hat-2.webp');
+}
+
+//
+
+function hover_living_fast_hoodie_1(element) {
+	element.setAttribute('src', '../images/collections/living-fast/hoodie-1-front.webp');
+	element.setAttribute('id', 'card');
+}
+
+function unhover_living_fast_hoodie_1(element) {
+	element.setAttribute('src', '../images/collections/living-fast/hoodie-1-back.webp');
+}
