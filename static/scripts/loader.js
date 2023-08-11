a5a30a3c5c11b773e97fb2fbe5b90f104ac81588
sababot <aitor@garciahombrados.com>
2023-08-11 18:54:30 +0200

    loader

---
+++ static/scripts/loader.js
@@ -0,0 +1,5 @@
+function showPage() {
+	document.getElementById("loader").style.opacity = 0;
+	document.getElementById("loader_div").style.opacity = 0;
+	document.getElementById("loader_div").style.zIndex = 0;
+}
