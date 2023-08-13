eaedb43b8783edecf88aeef27b114516a384c6a5
sababot <aitor@garciahombrados.com>
2023-08-13 13:41:10 +0200

    sidebar

---
+++ static/scripts/sidebar.js
@@ -0,0 +1,28 @@
+var sidebar = document.getElementById('sidebar');
+var show = false;
+
+function showSidebar() {
+	sidebar.style.transition = 'transform 0.5s';
+	sidebar.style.transform = 'translate(0vw)';
+	show = true;
+}
+
+function hideSidebar() {
+	setTimeout(disableTransition, 500);
+	sidebar.style.transform = 'translate(-100vw)';
+	show = false;
+}
+
+function disableTransition () {
+	sidebar.style.transition = '';
+}
+
+function toggleSidebar() {
+	if (show == false) {
+		showSidebar();
+	}
+
+	else {
+		hideSidebar();
+	}
+}
