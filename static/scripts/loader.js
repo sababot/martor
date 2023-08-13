659dc421c102ae05d6eaffa52a26aa287c0c5a2a
sababot <aitor@garciahombrados.com>
2023-08-13 14:08:43 +0200

    loader block bug

--- static/scripts/loader.js
+++ static/scripts/loader.js
@@ -1,5 +1,5 @@
 function showPage() {
 	document.getElementById("loader").style.opacity = 0;
 	document.getElementById("loader_div").style.opacity = 0;
-	document.getElementById("loader_div").style.zIndex = 0;
+	document.getElementById("loader_div").style.zIndex = -10;
 }
--- static/scripts/sidebar.js
+++ static/scripts/sidebar.js
@@ -4,12 +4,14 @@ var show = false;
 function showSidebar() {
 	sidebar.style.transition = 'transform 0.5s';
 	sidebar.style.transform = 'translate(0vw)';
+	sidebar.style.zIndex = "-5";
 	show = true;
 }
 
 function hideSidebar() {
 	setTimeout(disableTransition, 500);
 	sidebar.style.transform = 'translate(-100vw)';
+	sidebar.style.zIndex = "-50";
 	show = false;
 }
 
