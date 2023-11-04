409813a8fe03ffeebb37762cbc5fb694989072bd
sababot <aitor@garciahombrados.com>
2023-11-04 20:34:33 +0100

    new items for first collection

--- static/scripts/sidebar.js
+++ static/scripts/sidebar.js
@@ -2,7 +2,7 @@ var sidebar = document.getElementById('sidebar');
 var show = false;
 
 function showSidebar() {
-	sidebar.style.transition = 'transform 0.5s';
+	sidebar.style.transition = 'transform 0.25s';
 	sidebar.style.transform = 'translate(0vw)';
 	sidebar.style.zIndex = "-5";
 	show = true;
