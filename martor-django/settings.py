2b33ec36976c12d5d8221b022d3671868174d351
sababot <aitor@garciahombrados.com>
2024-10-27 18:00:54 +0100

    shop 2

--- martor-django/settings.py
+++ martor-django/settings.py
@@ -31,6 +31,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware'
+
+    'allauth.account.middleware.AccountMiddleware'
 ]
 
 ROOT_URLCONF = 'martor-django.urls'
